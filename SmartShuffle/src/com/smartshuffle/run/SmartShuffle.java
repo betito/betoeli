d25ee66fc682307ee42aa01117392857388ba5c2
betito.oliveira <betito.oliveira@91c6d495-2fb6-b5ce-2318-97b0d1a7af2b>
2014-02-23 18:55:11 +0000

    Initial tree.

---
+++ SmartShuffle/src/com/smartshuffle/run/SmartShuffle.java
@@ -0,0 +1,16 @@
+package com.smartshuffle.run;
+
+import com.smartshuffle.extract.*;
+
+public class SmartShuffle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		String audioPath = "musics\\Et-si-tu-n-existais-pas.mp3";
+		String audioPath = "";
+		new Extractor(audioPath);
+
+	}
+
+}
